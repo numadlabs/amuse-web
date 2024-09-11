63cdafd7a513039931227d921e2d1d0d5c92f9b8
dolgoonnn <dolgoon.da@gmail.com>
2024-09-11 11:26:00 +0800

    added app like page transition

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,22 +2,37 @@ import { ReactNode } from "react";
 import { Toaster } from "../ui/sonner";
 import Header from "./header-comp";
 import BottomNavigation from "./bottom-navigation";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 export default function AuthenticatedLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const router = useRouter();
+
   return (
     <div className="bg-background">
       <div
-        className={`flex flex-col w-full h-full  min-h-screen max-w-[600px] mx-auto items-center`}
+        className={`flex flex-col w-full h-full min-h-screen max-w-[600px] mx-auto items-center`}
       >
         <Header />
 
-        <div className="w-full">
+        <div className="w-full flex-grow">
           <div className="hidden lg:block">{/* <OnlyMobileWarning /> */}</div>
-          {children}
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={router.pathname}
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="flex-grow"
+            >
+              {children}
+            </motion.div>
+          </AnimatePresence>
           <Toaster />
         </div>
         <BottomNavigation />
